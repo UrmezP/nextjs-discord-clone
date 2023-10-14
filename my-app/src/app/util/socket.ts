92fd2f07e19c625ac9e56c532802e66ce3e65029
UrmezP <panthakiurmez@gmail.com>
2023-10-14 20:04:22 +0530

    Updated API from Dev to Prod

--- my-app/src/app/util/socket.ts
+++ my-app/src/app/util/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io(`${process.env.NEXT_PUBLIC_EXPRESS_SERVER_API_DEV}`);
+const socket = io(`${process.env.NEXT_PUBLIC_EXPRESS_SERVER_API_PROD}`);
 
 export default socket;
